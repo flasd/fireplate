b7237558932641956ffd4afdcb74be423999fe15
Marcel O. Coelho <flasd2@gmail.com>
2017-12-20 14:46:13 -0200

    Removed unnecessary bloat

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router';
-
-import './styles/reset.scss';
 import { HomeView } from './views';
 
 export default function App() {
     return (
         <div>
-            <div>Header</div>
-            <Route exact path="/" component={HomeView} />
+            <Route path="/" exact component={HomeView} />
         </div>
     );
 }
