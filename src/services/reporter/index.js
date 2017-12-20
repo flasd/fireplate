f14e995b3c87157a7a6f3e12386e3015223425be
Marcel O. Coelho <flasd2@gmail.com>
2017-12-20 14:50:39 -0200

    Redid all

--- src/services/reporter/index.js
+++ src/services/reporter/index.js
@@ -1,34 +1,23 @@
 import serializeError from 'serialize-error';
 import serializeJs from 'serialize-javascript';
 
-// import { database } from '../firebase';
+import { 
+    ERRORS_REFERENCE,
+    LOGS_REFERENCE,
+ } from '../firebase';
 
-// const errorReference = database().ref('private/errors');
 
-export function wrapError(err, inf) {
-    return {
-        error: serializeError(err),
-        info: serializeJs(inf),
-    };
+export function reportError(error, message) {
+    ERRORS_REFERENCE.push({
+        error: serializeError(error),
+        message: serializeJs(message),
+        timestamp: Date.now() || +new Date(),
+    });
 }
 
-export function report(error, info) {
-    const wrappedErr = wrapError(error, info || undefined);
-
-    // errorReference.push(wrappedErr)
-    // .catch(ex => null /* Shit can happen. */);
-
-    return error;
-}
-
-export function pipe(fn, ...args) {
-    try {
-        return fn(...args);
-    } catch (err) {
-        throw report(err);
-    }
-}
-
-export function pipeAsync(fn, ...args) {
-    return fn(...args).catch(err => Promise.reject(report(err)));
+export function reportInfo(info) {
+    LOGS_REFERENCE.push({
+        message: serializeJs(message),
+        timestamp: Date.now() || +new Date(),
+    });
 }
