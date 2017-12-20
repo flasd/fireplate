b6bbd9e644a5e8abdadb5f7c0bdc5e1617652686
Marcel O. Coelho <flasd2@gmail.com>
2017-12-20 15:01:07 -0200

    Removed unnecessary stiling

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import styles from './styles.scss';
 
 export default function HomeView() {
     return (
-        <div className={styles.container}>
+        <div>
             <Helmet>
-                <title>React App - Home</title>
+                <title>React App @ Home</title>
             </Helmet>
             HomeView
         </div>
