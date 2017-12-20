54a5866c7930a60e9423d4daa8ed50b3b54ea3a0
Marcel O. Coelho <flasd2@gmail.com>
2017-12-20 14:46:25 -0200

    Adapted file to work with new configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,5 @@
-const devConfig = './internals/webpack/webpack.dev';
-const prodConfig = './internals/webpack/webpack.prod';
-const servConfig = './internals/webpack/webpack.ssr';
-const dllConfig = './internals/webpack/webpack.dll';
-
-const env = process.env.NODE_ENV;
-const target = process.env.TARGET;
-
-if (env === 'production') {
-    module.exports = target === 'server' ? require(servConfig) : require(prodConfig);
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./webpack/prod.config');
 } else {
-    module.exports = target === 'dll' ? require(dllConfig) : require(devConfig);
+    module.exports = require('./webpack/dev.config');
 }
