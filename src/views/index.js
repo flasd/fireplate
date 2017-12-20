341da8ebf0d1c820618170a072ba7c861b2ae28b
Marcel O. Coelho <flasd2@gmail.com>
2017-12-20 14:58:27 -0200

    Added loading and error components

--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,28 @@
 import { asyncComponent } from 'react-async-component';
 
-export const HomeView = asyncComponent({
+import styles from './index.scss';
+
+export function LoadingComponent() {
+    return (
+        <div className={styles.loadingContainer} >
+            Loading...
+        </div>
+    );
+}
+export function ErrorComponent({ error }) {
+    return (
+        <div className={styles.errorContainer} >
+            { error.message }
+        </div>
+    );
+}
+export function getAsyncComponent(options) {
+    return asyncComponent(Object.assign({}, {
+        LoadingComponent,
+        ErrorComponent,
+    }, options));
+}
+export const HomeView = getAsyncComponent({
+    // eslint-disable-next-line
     resolve: () => import('./home'),
 });
