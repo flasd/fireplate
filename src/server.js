88b4399fceac7520881b56af99c874b935ca16c4
Marcel O. Coelho <flasd2@gmail.com>
2017-12-20 14:49:19 -0200

    Rewrite to make code more readable and compact

--- src/server.js
+++ src/server.js
@@ -1,70 +1,62 @@
 import React from 'react';
-import ReactHelmet from 'react-helmet';
 import asyncBootstrapper from 'react-async-bootstrapper';
+import reactHelmet from 'react-helmet';
 import serialize from 'serialize-javascript';
 import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router';
-import { AsyncComponentProvider, createAsyncContext } from 'react-async-component';
-import { readFileSync } from 'fs';
 import { renderToString } from 'react-dom/server';
-import { resolve } from 'path';
+
+import {
+    AsyncComponentProvider,
+    createAsyncContext,
+} from 'react-async-component';
 
 import App from './app';
-import store from './services/store';
+import htmlCanvas from './index.html';
+import store from './services/state';
+import { reportError } from './services/reporter';
 
-const templatePath = resolve(__dirname, './index.html');
-const template = readFileSync(templatePath).toString();
+export default async function renderApp(request, response) {
+    try {
+        const { url } = request;
 
+        if (url.indexOf('.') !== -1) {
+            return response.status(404).end();
+        }
 
-/**
- * Renders the app and ships it to the user
- *
- * @param      {express.Request}  request   The request,
- * @param      {express.Response}  response  The response,
- */
-export default function renderingMiddleware(request, response) {
-    if (request.url.indexOf('.') !== -1) {
-        return response.status(404).end();
+        const asyncContext = createAsyncContext();
+        const routerContext = {};
+
+        const Root = (
+            <AsyncComponentProvider asyncContext={asyncContext}>
+                <Provider store={store}>
+                    <StaticRouter location={url} context={routerContext}>
+                        <App />
+                    </StaticRouter>
+                </Provider>
+            </AsyncComponentProvider>
+        );
+
+        if (routerContext.redirectUrl) {
+            return response.redirect(302, routerContext.redirectUrl);
+        }
+
+        await asyncBootstrapper(Root);
+        const appMarkup = renderToString(Root);
+        const reduxState = store.getState();
+        const asyncState = asyncContext.getState();
+        const helmetState = reactHelmet.renderStatic();
+
+        const finalMarkup = htmlCanvas
+            .replace('<title></title>', helmetState.title.toString())
+            .replace('<!-- ::META:: -->', helmetState.meta.toString())
+            .replace('<!-- ::APP:: -->', appMarkup)
+            .replace('/* ::ASYNC_STATE:: */', serialize(asyncState))
+            .replace('/* ::REDUX_STATE:: */', serialize(reduxState));
+
+        return response.status(200).send(finalMarkup);
+    } catch (error) {
+        reportError(error, 'Error while trying to render the app.');
+        return response.status(500).end();
     }
-
-    return Promise.resolve()
-        .then(() => {
-            const asyncContext = createAsyncContext();
-            const routerContext = {};
-
-            const app = (
-                <AsyncComponentProvider asyncContext={asyncContext}>
-                    <Provider store={store}>
-                        <StaticRouter location={request.url} context={routerContext}>
-                            <App />
-                        </StaticRouter>
-                    </Provider>
-                </AsyncComponentProvider>
-            );
-
-            if (routerContext.url) {
-                return response.redirect(302, routerContext.url);
-            }
-
-            return asyncBootstrapper(app)
-                .then(() => {
-                    const appMarkup = renderToString(app);
-                    const reduxState = store.getState();
-                    const asyncState = asyncContext.getState();
-
-                    const helmetData = ReactHelmet.renderStatic();
-
-                    const html = template
-                        .replace('<title></title>', helmetData.title.toString())
-                        .replace('<!-- ::META:: -->', helmetData.meta.toString())
-                        .replace('<!-- ::APP:: -->', appMarkup)
-                        .replace('/* ::REDUX__STATE:: */', serialize(reduxState))
-                        .replace('/* ::ASYNC__STATE:: */', serialize(asyncState));
-
-                    response.status(200).send(html);
-                });
-        })
-        .catch(() => {
-            response.status(500).send('The server encountered an unexpected condition which prevented it from fulfilling the request.');
-        });
 }
