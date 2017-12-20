ab06de162a09666e411f2476ba876ec30a22fc26
Marcel O. Coelho <flasd2@gmail.com>
2017-12-20 14:49:40 -0200

    Only include analytics code on production builds

--- src/services/analytics/index.js
+++ src/services/analytics/index.js
@@ -1,13 +1,9 @@
 import { listen } from '../navigation';
 
-console.warn('Remember to update the details on the service/analytics');
-
-/* eslint-disable */
-
 if (process.env.NODE_ENV === 'production') {
-    (function(i, s, o, g, r, a, m) {
+    (function (i, s, o, g, r, a, m) {
         i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
+        i[r] = i[r] || function () {
             (i[r].q = i[r].q || []).push(arguments)
         }, i[r].l = 1 * new Date();
         a = s.createElement(o),
@@ -17,10 +13,10 @@ if (process.env.NODE_ENV === 'production') {
         m.parentNode.insertBefore(a, m)
     })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-    ga('create', '### YOUR ID ###', 'auto');
+    ga('create', process.env.ANALYTICS_ID, 'auto');
     ga('send', 'pageview');
 
     listen(location => ga('send', 'pageview', location.pathname));
+} else {
+    console.warn('Remember to update the details on the service/analytics');
 }
-
-/* eslint-enable */
