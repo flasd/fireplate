1076fbf019e903d324d94b86376558c22db98c1a
Marcel O. Coelho <flasd2@gmail.com>
2018-01-01 03:22:10 -0200

    Added common alias to the webpack resolver

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,8 +1,19 @@
+const path = require('path');
 const webpack = require('webpack');
+
+const resolve = partial => path.resolve(process.cwd(), partial);
 
 module.exports = {
     plugins: [
         new webpack.EnvironmentPlugin(['NODE_ENV']),
         new webpack.NamedModulesPlugin(),
     ],
+
+    resolve: {
+        alias: {
+            services: resolve('src/services'),
+            styles: resolve('src/styles'),
+            views: resolve('src/views'),
+        },
+    },
 };
