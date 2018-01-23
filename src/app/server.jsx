d1fe4795a9f760c905408f5f2071658956102c97
Marcel O. Coelho <flasd2@gmail.com>
2018-01-23 13:02:51 -0200

    Change extensions to JSX

--- src/app/server.jsx
+++ src/app/server.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import asyncBootstrapper from 'react-async-bootstrapper';
 import reactHelmet from 'react-helmet';
 import serialize from 'serialize-javascript';
+import fs from 'mz/fs';
+import path from 'path';
 import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router';
 import { renderToString } from 'react-dom/server';
@@ -12,20 +14,38 @@ import {
 } from 'react-async-component';
 
 import App from './app';
-import htmlCanvas from './index.html';
 import store from './services/state';
-import { reportError } from './services/reporter';
 
-export default async function renderApp(request, response) {
+const resolve = partial => path.resolve(process.cwd(), partial);
+
+export async function loadTemplate(request, response, next) {
+    try {
+        const htmlCanvas = await fs.readFile(resolve('./public/template.html'));
+        response.locals.htmlCanvas = htmlCanvas.toString();
+        next();
+    } catch (error) {
+        console.error('Error while trying to get HTML template', error.message);
+        response.status(500).end();
+    }
+}
+
+
+export async function renderApp(request, response) {
     try {
         const { url } = request;
 
         if (url.indexOf('.') !== -1) {
+            // If there's a '.' inside the url string, it means that the firebase
+            // static files didn't found any matches and delegate the request to
+            // the https function. Since we don't serve static files from the express
+            // app, we return a 404 here.
             return response.status(404).end();
         }
 
         const asyncContext = createAsyncContext();
-        const routerContext = {};
+        const routerContext = {
+            status: 200,
+        };
 
         const Root = (
             <AsyncComponentProvider asyncContext={asyncContext}>
@@ -37,8 +57,8 @@ export default async function renderApp(request, response) {
             </AsyncComponentProvider>
         );
 
-        if (routerContext.redirectUrl) {
-            return response.redirect(302, routerContext.redirectUrl);
+        if (routerContext.url) {
+            return response.redirect(routerContext.status, routerContext.url);
         }
 
         await asyncBootstrapper(Root);
@@ -47,16 +67,16 @@ export default async function renderApp(request, response) {
         const asyncState = asyncContext.getState();
         const helmetState = reactHelmet.renderStatic();
 
-        const finalMarkup = htmlCanvas
+        const finalMarkup = response.locals.htmlCanvas
             .replace('<title></title>', helmetState.title.toString())
             .replace('<!-- ::META:: -->', helmetState.meta.toString())
             .replace('<!-- ::APP:: -->', appMarkup)
             .replace('/* ::ASYNC_STATE:: */', serialize(asyncState))
             .replace('/* ::REDUX_STATE:: */', serialize(reduxState));
 
-        return response.status(200).send(finalMarkup);
+        return response.status(routerContext.status).send(finalMarkup);
     } catch (error) {
-        reportError(error, 'Error while trying to render the app.');
+        console.error('Error while rendering the app', error.message);
         return response.status(500).end();
     }
 }
