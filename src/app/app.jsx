55e59fb5056ae2f72e23bbc671b32c11ba2ea7ea
Marcel O. Coelho <flasd2@gmail.com>
2018-01-23 13:01:56 -0200

    Change extensions to JSX and added a Switch component to be able to render a 404 route

---
+++ src/app/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+
+import './styles/app.scss';
+import './services/analytics';
+import './services/service-worker';
+import Home from './views/home';
+import NotFound from './views/not-found';
+
+export default function App() {
+    return (
+        <main>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route component={NotFound} />
+            </Switch>
+        </main>
+    );
+}
