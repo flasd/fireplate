824404adebd814d579e0d14f6ec9464f4c0169fc
Marcel O. Coelho <flasd2@gmail.com>
2018-01-23 12:50:47 -0200

    Added all loaders and shared plugins into the base config

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,19 +1,47 @@
 const path = require('path');
-const webpack = require('webpack');
+
+const babelLoader = require('./loaders/babel.loader');
+const fontsLoader = require('./loaders/fonts.loader');
+const imagesLoader = require('./loaders/image.loader');
+const scssLoader = require('./loaders/scss.loader');
+
+const baseHrefPlugin = require('./plugins/base-href.plugin');
+const circularDependenciePlugin = require('./plugins/circular-dependency.plugin');
+const environmentPlugin = require('./plugins/environment.plugin');
+const extractTextPlugin = require('./plugins/extract-text.plugin');
+const htmlPlugin = require('./plugins/html.plugin');
+const noEmitOnErrorPlugin = require('./plugins/no-emit-on-error.plugin');
+
 
 const resolve = partial => path.resolve(process.cwd(), partial);
 
+
 module.exports = {
+    module: {
+        rules: [
+            babelLoader,
+            fontsLoader,
+            imagesLoader,
+            scssLoader,
+        ],
+    },
+
     plugins: [
-        new webpack.EnvironmentPlugin(['NODE_ENV']),
-        new webpack.NamedModulesPlugin(),
+        baseHrefPlugin,
+        circularDependenciePlugin,
+        environmentPlugin,
+        extractTextPlugin,
+        htmlPlugin,
+        noEmitOnErrorPlugin,
     ],
 
     resolve: {
         alias: {
-            services: resolve('src/services'),
-            styles: resolve('src/styles'),
-            views: resolve('src/views'),
+            app: resolve('src/app'),
+            services: resolve('src/app/services'),
+            styles: resolve('src/app/styles'),
         },
+
+        extensions: ['.js', '.jsx', '.json'],
     },
 };
