96e1c5d8017e0535df52c21dcacbb293b43fb517
Marcel O. Coelho <flasd2@gmail.com>
2018-01-23 14:02:37 -0200

    Updated service-worker install mechanism

--- src/app/services/service-worker/index.js
+++ src/app/services/service-worker/index.js
@@ -1,11 +1,6 @@
 (() => {
     if (process.env.BUILD_TARGET !== 'node' && process.env.NODE_ENV === 'production') {
-        /* eslint-env browser */
-        try {
-            if ('serviceWorker' in navigator) {
-                navigator.serviceWorker.register('/service-worker.js');
-            }
-            // eslint-disable-next-line no-empty
-        } catch (x) {}
+        /* eslint-disable global-require, import/no-extraneous-dependencies */
+        require('offline-plugin/runtime').install();
     }
 })();
