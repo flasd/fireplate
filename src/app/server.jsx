a62892664f0905a887bd7d0d31600b06be505269
Marcel O. Coelho <flasd2@gmail.com>
2018-01-31 23:44:07 -0200

    Moved loadTemplate middleware to its own file

--- src/app/server.jsx
+++ src/app/server.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import asyncBootstrapper from 'react-async-bootstrapper';
 import reactHelmet from 'react-helmet';
 import serialize from 'serialize-javascript';
-import fs from 'mz/fs';
-import path from 'path';
 import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router';
 import { renderToString } from 'react-dom/server';
@@ -16,31 +14,9 @@ import {
 import App from './app';
 import store from './services/state';
 
-const resolve = partial => path.resolve(process.cwd(), partial);
-
-export async function loadTemplate(request, response, next) {
-    try {
-        const htmlCanvas = await fs.readFile(resolve('./public/template.html'));
-        response.locals.htmlCanvas = htmlCanvas.toString();
-        next();
-    } catch (error) {
-        console.error('Error while trying to get HTML template', error.message);
-        response.status(500).end();
-    }
-}
-
-
-export async function renderApp(request, response) {
+export default async function renderApp(request, response) {
     try {
         const { url } = request;
-
-        if (url.indexOf('.') !== -1) {
-            // If there's a '.' inside the url string, it means that the firebase
-            // static files didn't found any matches and delegate the request to
-            // the https function. Since we don't serve static files from the express
-            // app, we return a 404 here.
-            return response.status(404).end();
-        }
 
         const asyncContext = createAsyncContext();
         const routerContext = {
