2b79a9261b19336932021a43da3c562ba884321f
Marcel O. Coelho <flasd2@gmail.com>
2018-01-31 23:43:22 -0200

    Added missing async bootstraping

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,29 +1,38 @@
 /* eslint-env browser */
 import React from 'react';
+import asyncBootstrapper from 'react-async-bootstrapper';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import { render } from 'react-dom';
+import { AsyncComponentProvider } from 'react-async-component';
 
-import App from './app.jsx';
+import App from './app';
 
 import history from './services/navigation';
 import store from './services/state';
 
 export default function getRoot(CurrentApp) {
     return () => (
-        <Provider store={store}>
-            <Router history={history}>
-                <CurrentApp />
-            </Router>
-        </Provider>
+        <AsyncComponentProvider rehydrateState={window.ASYNC_COMPONENTS_STATE || {}}>
+            <Provider store={store}>
+                <Router history={history}>
+                    <CurrentApp />
+                </Router>
+            </Provider>
+        </AsyncComponentProvider>
     );
 }
 
 if (process.env.NODE_ENV === 'development' && module && module.hot) {
     module.hot.accept('./app.jsx', () => {
+        // eslint-disable-next-line global-require
         const CurrentApp = require('./app.jsx').default;
         render(getRoot(CurrentApp)(), document.getElementById('app'));
     });
 }
 
-render(getRoot(App)(), document.getElementById('app'));
+const EntryApp = getRoot(App)();
+
+asyncBootstrapper(EntryApp).then(() => {
+    render(EntryApp, document.getElementById('app'));
+});
