5fb9ce3089f6c68bc620a61945d30941834bf5a7
Marcel O. Coelho <flasd2@gmail.com>
2018-02-04 15:12:15 -0200

    Keep non-critical path css inside component modules

--- webpack/loaders/scss.loader.js
+++ webpack/loaders/scss.loader.js
@@ -1,53 +1,70 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
-const onDevelopment = process.env.NODE_ENV === 'development';
-
+const onDevelopment = process.env.NODE_ENV !== 'production';
+const extractExternalCss = require('../plugins/extract-text.plugin');
 
 const resolve = partial => path.resolve(process.cwd(), partial);
 
+const asyncCssLoader = [{
+    loader: 'style-loader/url',
+}, {
+    loader: 'file-loader',
+    options: {
+        name: 'stylesheets/[hash].css',
+    },
+}, {
+    loader: 'extract-loader',
+}];
+
 const styleLoader = [{
+    loader: 'style-loader',
+}];
+
+const isomorphicStyleLoader = [{
     loader: 'isomorphic-style-loader',
 }];
 
-const loaderChain = [
-    {
-        loader: 'css-loader',
-        options: {
-            sourceMap: onDevelopment,
-            modules: true,
-            localIdentName: '[hash:base64:5]',
-            importLoaders: 3,
-        },
-    }, {
-        loader: 'postcss-loader',
-        options: {
-            sourceMap: onDevelopment,
-        },
-    }, {
-        loader: 'resolve-url-loader',
-        options: {
-            sourceMap: onDevelopment,
-            attempts: 1,
-            fail: true,
-        }
-    }, {
-        loader: 'sass-loader',
-        options: {
-            includePaths: [
-                resolve('src/app/styles'),
-                require('bourbon').includePaths,
-                resolve('node_modules/normalize-scss/sass'),
-            ],
-            sourceMap: true,
-        },
+const loaderChain = [{
+    loader: 'css-loader',
+    options: {
+        sourceMap: onDevelopment,
+        modules: true,
+        localIdentName: '[hash:base64:5]',
+        importLoaders: 3,
+    },
+}, {
+    loader: 'postcss-loader',
+    options: {
+        sourceMap: onDevelopment,
+    },
+}, {
+    loader: 'resolve-url-loader',
+    options: {
+        sourceMap: onDevelopment,
+        attempts: 1,
+        fail: true,
     }
-]
+}, {
+    loader: 'sass-loader',
+    options: {
+        includePaths: [
+            resolve('src/app/styles'),
+            require('bourbon').includePaths,
+            resolve('node_modules/normalize-scss/sass'),
+        ],
+        sourceMap: true,
+    },
+}];
+
+function getLoader() {
+    if (process.env.BUILD_TARGET === 'node') {
+        return isomorphicStyleLoader.concat(loaderChain);
+    }
+
+    return styleLoader.concat(loaderChain);
+}
 
 module.exports = {
-    test: /\.scss$/,
+    test: /^((?!critical|async).)*\.scss$/,
     exclude: /(node_modules|bower_components)/,
-    use: onDevelopment ? styleLoader.concat(loaderChain) : ExtractTextPlugin.extract({
-        use: loaderChain,
-        fallback: styleLoader,
-    }),
+    use: getLoader(),
 };
