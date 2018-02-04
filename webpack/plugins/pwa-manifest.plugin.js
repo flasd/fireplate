63cd7cb00f474b3fbea0600516c7338ee154fb48
Marcel O. Coelho <flasd2@gmail.com>
2018-02-04 15:07:56 -0200

    Added more info to the pwa manifest

--- webpack/plugins/pwa-manifest.plugin.js
+++ webpack/plugins/pwa-manifest.plugin.js
@@ -4,10 +4,19 @@ const path = require('path');
 const resolve = partial => path.resolve(process.cwd(), partial);
 
 module.exports = new WebpackPwaManifest({
-    name: 'Fireplate App',
-    short_name: 'FPApp',
+    background_color: '#341539',
     description: 'PWAs are awesome son.',
-    background_color: '#ffffff',
+    developerName: 'Marcel O. Coelho',
+    developerURL: 'https://github.com/flasd',
+    display: 'standalone',
+    name: 'Fireplate',
+    orientation: 'portrait',
+    short_name: 'Fireplate',
+    start_url: '/?homescreen=1',
+    theme_color: '#341539',
+
+    inject: true,
+    ios: true,
     icons: [{
         src: resolve('src/app/assets/icons/ios-icon.png'),
         sizes: [120, 152, 167, 180, 1024],
