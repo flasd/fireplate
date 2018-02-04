42a882fba4695ba9438b107ec919fdc1c69939bf
Marcel O. Coelho <flasd2@gmail.com>
2018-02-04 15:58:11 -0200

    Tested navigation service export

--- src/app/services/navigation/index.test.js
+++ src/app/services/navigation/index.test.js
@@ -1,6 +1,18 @@
 /* eslint-env node, es6, mocha */
 
+import { expect } from 'chai';
 
-describe('Navitarion Service', () => {
-    it('should export the right properties');
+import history from './index';
+
+describe('App / Service / Navitarion', () => {
+    it('should export the right properties', () => {
+        expect(history).to.be.a('object');
+        expect(history).to.have.property('listen').which.is.a('function');
+        expect(history).to.have.property('push').which.is.a('function');
+        expect(history).to.have.property('replace').which.is.a('function');
+        expect(history).to.have.property('go').which.is.a('function');
+        expect(history).to.have.property('goBack').which.is.a('function');
+        expect(history).to.have.property('goForward').which.is.a('function');
+        expect(history).to.have.property('canGo').which.is.a('function');
+    });
 });
