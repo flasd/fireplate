b551dcd26acfb858a85b7bfeb56269b85e9d4126
Marcel O. Coelho <flasd2@gmail.com>
2018-02-04 15:06:41 -0200

    Extract and inline critical path css

---
+++ webpack/loaders/critical-style.loader.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const onDevelopment = process.env.NODE_ENV !== 'production';
+const extractTextInstance = require('../plugins/extract-text.plugin');
+
+const resolve = partial => path.resolve(process.cwd(), partial);
+
+const styleLoader = [{
+    loader: 'isomorphic-style-loader',
+}];
+
+const loaderChain = [
+    {
+        loader: 'css-loader',
+        options: {
+            sourceMap: onDevelopment,
+            modules: true,
+            localIdentName: '[hash:base64:5]',
+            importLoaders: 3,
+        },
+    }, {
+        loader: 'postcss-loader',
+        options: {
+            sourceMap: onDevelopment,
+        },
+    }, {
+        loader: 'resolve-url-loader',
+        options: {
+            sourceMap: onDevelopment,
+            attempts: 1,
+            fail: true,
+        }
+    }, {
+        loader: 'sass-loader',
+        options: {
+            includePaths: [
+                resolve('src/app/styles'),
+                require('bourbon').includePaths,
+                resolve('node_modules/normalize-scss/sass'),
+            ],
+            sourceMap: true,
+        },
+    }
+]
+
+module.exports = {
+    test: /\.critical\.scss$/,
+    exclude: /(node_modules|bower_components)/,
+    use: onDevelopment ? styleLoader.concat(loaderChain) : extractTextInstance.extract({
+        use: loaderChain,
+        fallback: styleLoader,
+    }),
+};
---
+++ webpack/plugins/style-ext-html.plugin.js
@@ -0,0 +1,3 @@
+const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
+
+module.exports = new StyleExtHtmlWebpackPlugin('stylesheets/critical.css');
