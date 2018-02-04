101ce57f3a4fe18acbc5afa35fb4538ee85bf949
Marcel O. Coelho <flasd2@gmail.com>
2018-02-04 15:58:38 -0200

    Started testing redux logic

--- src/app/services/state/index.test.js
+++ src/app/services/state/index.test.js
@@ -1,6 +1,16 @@
 /* eslint-env node, es6, mocha */
 
+import { expect } from 'chai';
+
+import store from './index';
 
 describe('Redux Service', () => {
-    it('should export a valid redux store');
+    it('should apply store enhancers when running on the browser in prod env');
+    it('should export a valid redux store', () => {
+        expect(store).to.be.a('object');
+        expect(store).to.have.property('dispatch').which.is.a('function');
+        expect(store).to.have.property('subscribe').which.is.a('function');
+        expect(store).to.have.property('getState').which.is.a('function');
+        expect(store).to.have.property('replaceReducer').which.is.a('function');
+    });
 });
--- src/app/services/state/reducer.test.js
+++ src/app/services/state/reducer.test.js
@@ -1,6 +1,13 @@
 /* eslint-env node, es6, mocha */
 
+import { expect } from 'chai';
+
+import reducer from './reducer';
 
 describe('Redux rootReducer', () => {
-    it('should reduce actions properly');
+    it('should reduce actions properly', () => {
+        expect(reducer).to.be.a('function');
+        expect(reducer()).to.equal('hello');
+        expect(reducer('bye')).to.equal('bye');
+    });
 });
