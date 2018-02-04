264695918644d029487591b93a42d55951d9f362
Marcel O. Coelho <flasd2@gmail.com>
2018-02-04 15:58:23 -0200

    Tested service worker installing logic

--- src/app/services/service-worker/index.test.js
+++ src/app/services/service-worker/index.test.js
@@ -1,6 +1,62 @@
-/* eslint-env node, es6, mocha */
+/* eslint-disable */
+
+import proxyquire from 'proxyquire';
+import { expect } from 'chai';
+import { spy } from 'sinon';
 
 
-describe('ServiceWorker Service', () => {
-    it('should install the service worker only during production');
+function setEnv(newProps) {
+    const oldEnv = Object.assign({}, process.env);
+    process.env = Object.assign({}, process.env, newProps);
+
+    return () => {
+        process.env = oldEnv;
+    };
+}
+
+describe('App / Service / Service Worker', () => {
+    it('should do nothing in node env', () => {
+        const resetEnv = setEnv({ BUILD_TARGET: 'node' });
+
+        const installSpy = spy();
+        const offlinePluginStub = { install: installSpy };
+
+        proxyquire.noPreserveCache().noCallThru().load('./index.js', {
+            'offline-plugin/runtime': offlinePluginStub,
+        });
+
+        expect(installSpy.called).to.be.false;
+
+        resetEnv();
+    });
+
+    it('should do nothing in developer env', () => {
+        const resetEnv = setEnv({ NODE_ENV: 'development'});
+
+        const installSpy = spy();
+        const offlinePluginStub = { install: installSpy };
+
+        proxyquire.noPreserveCache().noCallThru().load('./index.js', {
+            'offline-plugin/runtime': offlinePluginStub,
+        });
+
+        expect(installSpy.called).to.be.false;
+
+        resetEnv();
+    });
+
+    it('should install the service worker when running on production in the browser', () => {
+        const resetEnv = setEnv({ NODE_ENV: 'production', BUILD_TARGET: 'browser' });
+
+        const installSpy = spy();
+        const offlinePluginStub = { install: installSpy };
+
+        proxyquire.noPreserveCache().noCallThru().load('./index.js', {
+            'offline-plugin/runtime': offlinePluginStub,
+        });
+
+        expect(installSpy.calledOnce).to.be.true;
+
+        resetEnv();
+    });
 });
