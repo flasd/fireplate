78c85724e0b78cb81ac53569d9520c92bba9ee8c
Marcel O. Coelho <flasd2@gmail.com>
2018-02-07 20:13:39 -0200

    Made env changing function async

--- src/app/services/service-worker/index.test.js
+++ src/app/services/service-worker/index.test.js
@@ -4,17 +4,25 @@ import { spy } from 'sinon';
 
 
 function setEnv(newProps) {
-    const oldEnv = Object.assign({}, process.env);
-    process.env = Object.assign({}, process.env, newProps);
+    const oldEnv = {};
 
-    return () => {
-        process.env = oldEnv;
-    };
+    Object.keys(newProps).forEach(key => {
+        oldEnv[key] = newProps[key];
+        process.env[key] = newProps[key];
+    });
+
+    return Promise.resolve(() => {
+        return Promise.resolve(() => {
+            Object.keys(oldEnv).forEach(key => {
+                process.env[key] = oldEnv[key];
+            });
+        });
+    });
 }
 
 describe('App / Service / Service Worker', () => {
-    it('should do nothing in node env', () => {
-        const resetEnv = setEnv({ BUILD_TARGET: 'node' });
+    it('should do nothing in node env', async () => {
+        const resetEnv = await setEnv({ BUILD_TARGET: 'node' });
 
         const installSpy = spy();
         const offlinePluginStub = { install: installSpy };
@@ -25,11 +33,11 @@ describe('App / Service / Service Worker', () => {
 
         expect(installSpy.called).to.be.false;
 
-        resetEnv();
+        await resetEnv();
     });
 
-    it('should do nothing in developer env', () => {
-        const resetEnv = setEnv({ NODE_ENV: 'development'});
+    it('should do nothing in developer env', async () => {
+        const resetEnv = await setEnv({ NODE_ENV: 'development'});
 
         const installSpy = spy();
         const offlinePluginStub = { install: installSpy };
@@ -40,11 +48,11 @@ describe('App / Service / Service Worker', () => {
 
         expect(installSpy.called).to.be.false;
 
-        resetEnv();
+        await resetEnv();
     });
 
-    it('should install the service worker when running on production in the browser', () => {
-        const resetEnv = setEnv({ NODE_ENV: 'production', BUILD_TARGET: 'browser' });
+    it('should install the service worker when running on production in the browser', async () => {
+        const resetEnv = await setEnv({ NODE_ENV: 'production', BUILD_TARGET: 'browser' });
 
         const installSpy = spy();
         const offlinePluginStub = { install: installSpy };
@@ -55,6 +63,6 @@ describe('App / Service / Service Worker', () => {
 
         expect(installSpy.calledOnce).to.be.true;
 
-        resetEnv();
+        await resetEnv();
     });
 });
--- src/app/services/state/index.test.js
+++ src/app/services/state/index.test.js
@@ -3,18 +3,29 @@ import { expect } from 'chai';
 import { spy, stub } from 'sinon';
 
 function setEnv(newProps) {
-    const oldEnv = Object.assign({}, process.env);
-    process.env = Object.assign({}, process.env, newProps);
+    const oldEnv = {};
 
-    return () => {
-        process.env = oldEnv;
-    };
+    Object.keys(newProps).forEach(key => {
+        oldEnv[key] = newProps[key];
+        process.env[key] = newProps[key];
+    });
+
+    return Promise.resolve(() => {
+        return Promise.resolve(() => {
+            Object.keys(oldEnv).forEach(key => {
+                process.env[key] = oldEnv[key];
+            });
+        });
+    });
 }
 
 describe('App / Service / Redux', () => {
-    it('should decorated the store when running on the browser', (done) => {
-        const resetEnv = setEnv({ BUILD_TARGET: 'browser' });
+    before(() => {
         global.window = {};
+    });
+
+    it('should decorated the store when running on the browser', async () => {
+        const resetEnv = await setEnv({ BUILD_TARGET: 'browser' });
 
         const promiseMiddlewareSpy = spy();
         const applyMiddlewareSpy = spy();
@@ -35,20 +46,21 @@ describe('App / Service / Redux', () => {
         expect(composeSpy.called).to.be.false;
         expect(createStoreStub.calledOnce).to.be.true;
 
-        delete global.window;
-        resetEnv();
-        done();
+        await resetEnv();
     });
 
     it('should expose the store to the redux devtool during development');
 
-    it('should export a valid redux store', (done) => {
+    it('should export a valid redux store', () => {
         const store = require('./index').default;
         expect(store).to.be.a('object');
         expect(store).to.have.property('dispatch').which.is.a('function');
         expect(store).to.have.property('subscribe').which.is.a('function');
         expect(store).to.have.property('getState').which.is.a('function');
         expect(store).to.have.property('replaceReducer').which.is.a('function');
-        done();
+    });
+
+    after(() => {
+        delete global.window;
     });
 });
