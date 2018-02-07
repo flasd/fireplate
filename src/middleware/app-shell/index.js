e8e7bd981e9d8bc1c212c243e844f467e1abf8cd
Marcel O. Coelho <flasd2@gmail.com>
2018-02-07 13:36:36 -0200

    Fixed missing toString buffer convertion, fixed response.send command added \n on the end of the error message

--- src/middleware/app-shell/index.js
+++ src/middleware/app-shell/index.js
@@ -7,15 +7,15 @@ const resolve = partial => path.resolve(process.cwd(), partial);
 export default async function serveAppShell(request, response) {
     try {
         const htmlCanvas = await fs.readFile(resolve('./template.html'));
-        const markup = htmlCanvas.replace('<title></title>', 'Fireplate App Shell')
+        const markup = htmlCanvas.toString().replace('<title></title>', 'Fireplate App Shell')
             .replace('<!-- ::META:: -->', '')
             .replace('<!-- ::APP:: -->', '')
             .replace('/* ::ASYNC_STATE:: */', serialize({ resolved: {} }))
             .replace('/* ::REDUX_STATE:: */', serialize({}));
 
-        response.html(markup);
+        response.send(markup);
     } catch (error) {
-        console.error('Error while trying to get HTML template', error.message);
+        console.error('Error while trying to get HTML template.\n', error);
         response.status(500).end();
     }
 }
