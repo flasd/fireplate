4d274178f8780961759f12fbd8b1ace066208d22
Marcel O. Coelho <flasd2@gmail.com>
2018-02-08 15:10:32 -0200

    Tested the redirect component

--- src/app/components/Redirect/index.test.js
+++ src/app/components/Redirect/index.test.js
@@ -1,4 +1,17 @@
-describe('Component / Redirect', () => {
-    it('should render without exploding');
-    it('should export the right component');
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import Container from './index';
+import Redirect from './Redirect';
+
+describe('App / Components / Redirect', () => {
+    it('should render without exploding', () => {
+        expect(shallow(<Container />)).to.be.ok;
+    });
+
+    it('should export the Redirect component', () => {
+        const wrapper = shallow(<Container />);
+        expect(<Container />).to.deep.equal(<Redirect />);
+    });
 });
