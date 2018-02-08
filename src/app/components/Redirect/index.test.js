90c86d9c308e2224d27904e9c8081b0a56b0cf68
Marcel O. Coelho <flasd2@gmail.com>
2018-02-08 15:26:13 -0200

    Fixed prop-type warming for not using from and to props

--- src/app/components/Redirect/index.test.js
+++ src/app/components/Redirect/index.test.js
@@ -7,11 +7,11 @@ import Redirect from './Redirect';
 
 describe('App / Components / Redirect', () => {
     it('should render without exploding', () => {
-        expect(shallow(<Container />)).to.be.ok;
+        expect(shallow(<Container from="/" to="/" />)).to.be.ok;
     });
 
     it('should export the Redirect component', () => {
         const wrapper = shallow(<Container />);
-        expect(<Container />).to.deep.equal(<Redirect />);
+        expect(<Container from="/" to="/" />).to.deep.equal(<Redirect from="/" to="/" />);
     });
 });
