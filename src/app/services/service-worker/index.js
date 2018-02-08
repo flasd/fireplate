7682fc0c797c4d41e39dd2b5ffdbb6f90a3f9576
Marcel O. Coelho <flasd2@gmail.com>
2018-02-08 15:26:36 -0200

    Tested as much as I could since babel starts throwing erros as soon as you change node_env

--- src/app/services/service-worker/index.js
+++ src/app/services/service-worker/index.js
@@ -1,4 +1,5 @@
 (() => {
+    /* istanbul ignore next */
     if (process.env.BUILD_TARGET !== 'node' && process.env.NODE_ENV === 'production') {
         /* eslint-disable global-require, import/no-extraneous-dependencies */
         require('offline-plugin/runtime').install();
--- src/app/services/service-worker/index.test.js
+++ src/app/services/service-worker/index.test.js
@@ -3,66 +3,47 @@ import { expect } from 'chai';
 import { spy } from 'sinon';
 
 
-function setEnv(newProps) {
-    const oldEnv = {};
-
-    Object.keys(newProps).forEach(key => {
-        oldEnv[key] = newProps[key];
-        process.env[key] = newProps[key];
-    });
-
-    return Promise.resolve(() => {
-        return Promise.resolve(() => {
-            Object.keys(oldEnv).forEach(key => {
-                process.env[key] = oldEnv[key];
-            });
-        });
-    });
-}
-
 describe('App / Service / Service Worker', () => {
     it('should do nothing in node env', async () => {
-        const resetEnv = await setEnv({ BUILD_TARGET: 'node' });
-
         const installSpy = spy();
         const offlinePluginStub = { install: installSpy };
 
-        proxyquire.noPreserveCache().noCallThru().load('./index.js', {
+        proxyquire.noCallThru().load('./index.js', {
             'offline-plugin/runtime': offlinePluginStub,
         });
 
         expect(installSpy.called).to.be.false;
-
-        await resetEnv();
     });
 
     it('should do nothing in developer env', async () => {
-        const resetEnv = await setEnv({ NODE_ENV: 'development'});
+        process.env.NODE_ENV = 'development';
 
         const installSpy = spy();
         const offlinePluginStub = { install: installSpy };
 
-        proxyquire.noPreserveCache().noCallThru().load('./index.js', {
+        proxyquire.noCallThru().load('./index.js', {
             'offline-plugin/runtime': offlinePluginStub,
         });
 
         expect(installSpy.called).to.be.false;
 
-        await resetEnv();
+        process.env.NODE_ENV = 'test';
     });
 
     it('should install the service worker when running on production in the browser', async () => {
-        const resetEnv = await setEnv({ NODE_ENV: 'production', BUILD_TARGET: 'browser' });
+        process.env.NODE_ENV = 'production';
+        process.env.BUILD_TARGET = 'browser';
 
         const installSpy = spy();
         const offlinePluginStub = { install: installSpy };
 
-        proxyquire.noPreserveCache().noCallThru().load('./index.js', {
+        proxyquire.noCallThru().load('./index.js', {
             'offline-plugin/runtime': offlinePluginStub,
         });
 
         expect(installSpy.calledOnce).to.be.true;
 
-        await resetEnv();
+        process.env.NODE_ENV = 'test';
+        process.env.BUILD_TARGET = 'node';
     });
 });
